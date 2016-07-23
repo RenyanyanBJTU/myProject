68cf99514b406ae2a6205ae56e4d85e18cfb6437
RenyanyanBJTU <15120433@bjtu.edu.cn>
2016-07-23 12:46:59 +0800

    Update autosh.sh

--- android/auto-mount-diff/autosh.sh
+++ android/auto-mount-diff/autosh.sh
@@ -1,55 +1,156 @@
 #!/bin/bash
 
-# this shell script is to auto mount and get info about files in android.raw 
+# this shell script is to auto mount and get info 
+#about files in android installed disk(format qcow2 or raw) 
+
+if [ $# -ne 2 ];then
+  echo "please input two android installed disk name"
+  exit 1
+fi
 
 #install software
 sudo apt-get install tree kpartx
 
-#mount android-4.2.raw
-sudo losetup /dev/loop0 android-4.2.raw
+
+#mount android-4.2 and get source folder
+sudo mkdir /mnt/android4.2
+
+sudo losetup /dev/loop0 $1
 sudo kpartx -av /dev/loop0
 sudo mount /dev/mapper/loop0p1 /mnt/android4.2
+
 if [ $? -ne 0 ];then
-  echo "mount android4.2 faild.program running stoped."
-  exit 1
+  echo "changing mount method......"
+  sudo kpartx -dv /dev/loop0
+  sudo losetup -d /dev/loop0
+
+  sudo modprobe nbd max_part=8
+
+  sudo qemu-nbd -c /dev/nbd0 $1
+  sudo mount /dev/nbd0p1 /mnt/android4.2
+  
+  if [ $? -ne 0 ];then
+    echo "mount android4.2 faild.program running stoped."
+    exit 1
+  else
+    echo "mount android4.2 successful."
+  fi
+   
+  # copy file form /mnt to current dir
+  echo "copy /mnt/android4.2 to current dir...."
+  sudo cp -R /mnt/android4.2/. android4.2
+  if [ $? -ne 0 ];then
+    echo "copy faild.program running stoped."
+    exit 1
+  else
+    echo "copy successful."
+  fi
+ 
+  #release source
+  sudo umount /mnt/android4.2
+  sudo killall qemu-nbd ##
+  if [ $? -ne 0 ];then
+    echo "release qemu-nbd faild.program running stoped."
+    exit 1
+  else
+    echo "release qemu-nbd successful."
+  fi
+
 else
   echo "mount android4.2 successful."
+ 
+  # copy file form /mnt to current dir
+  echo "copy /mnt/android4.2 to current dir...."
+  sudo cp -R /mnt/android4.2/. android4.2
+  if [ $? -ne 0 ];then
+    echo "copy faild.program running stoped."
+    exit 1
+  else
+    echo "copy successful."
+  fi
+  
+  #release mount source
+  sudo umount /mnt/android4.2
+  sudo kpartx -dv /dev/loop0
+  sudo losetup -d /dev/loop0
+
+  if [ $? -ne 0 ];then
+    echo "release loop faild.program running stoped."
+    exit 1
+  else
+    echo "release loop successful."
+  fi
 fi
 
-#mount android-4.3.raw
-sudo losetup /dev/loop1 android-4.3.raw
-sudo kpartx -av /dev/loop1
-sudo mount /dev/mapper/loop1p1 /mnt/android4.3
+
+#mount android-4.3 and get source folder
+sudo mkdir /mnt/android4.3
+
+sudo losetup /dev/loop0 $2
+sudo kpartx -av /dev/loop0
+sudo mount /dev/mapper/loop0p1 /mnt/android4.3
+
 if [ $? -ne 0 ];then
-  echo "mount android4.3 faild.program running stoped."
-  exit 1
+  echo "changing mount method......"
+  sudo kpartx -dv /dev/loop0
+  sudo losetup -d /dev/loop0
+
+  sudo modprobe nbd max_part=8
+
+  sudo qemu-nbd -c /dev/nbd0 $2
+  sudo mount /dev/nbd0p1 /mnt/android4.3
+  
+  if [ $? -ne 0 ];then
+    echo "mount android4.3 faild.program running stoped."
+    exit 1
+  else
+    echo "mount android4.3 successful."
+  fi
+   
+  # copy file form /mnt to current dir
+  echo "copy /mnt/android4.3 to current dir...."
+  sudo cp -R /mnt/android4.3/. android4.3
+  if [ $? -ne 0 ];then
+    echo "copy faild.program running stoped."
+    exit 1
+  else
+    echo "copy successful."
+  fi
+ 
+  #release source
+  sudo umount /mnt/android4.3
+  sudo killall qemu-nbd ##
+  if [ $? -ne 0 ];then
+    echo "release qemu-nbd faild.program running stoped."
+    exit 1
+  else
+    echo "release qemu-nbd successful."
+  fi
+
 else
   echo "mount android4.3 successful."
-fi
+ 
+  # copy file form /mnt to current dir
+  echo "copy /mnt/android4.3 to current dir...."
+  sudo cp -R /mnt/android4.2/. android4.2
+  if [ $? -ne 0 ];then
+    echo "copy faild.program running stoped."
+    exit 1
+  else
+    echo "copy successful."
+  fi
+  
+  #release mount source
+  sudo umount /mnt/android4.3
+  sudo kpartx -dv /dev/loop0
+  sudo losetup -d /dev/loop0
 
-# copy file form /mnt to current dir
-echo "copy /mnt/.. to current dir...."
-sudo cp -R /mnt/android4.2/. android4.2
-sudo cp -R /mnt/android4.3/. android4.3
-if [ $? -ne 0 ];then
-  echo "copy faild.program running stoped."
-  exit 1
-else
-  echo "copy successful."
-fi
-
-#release mount source
-sudo umount /mnt/android4.2
-sudo kpartx -dv /dev/loop0
-sudo losetup -d /dev/loop0
-sudo umount /mnt/android4.3
-sudo kpartx -dv /dev/loop1
-sudo losetup -d /dev/loop1
-if [ $? -ne 0 ];then
-  echo "release loop faild.program running stoped."
-  exit 1
-else
-  echo "release loop successful."
+  if [ $? -ne 0 ];then
+    echo "release loop faild.program running stoped."
+    exit 1
+  else
+    echo "release loop successful."
+  fi
 fi
 
 #get android4.2 file system
@@ -92,7 +193,6 @@ if [ $? -ne 0 ];then
 else
  echo "treesh.sh program running successful."
 fi
-
 
 
 
